64628062c0fa8912818045ab7276dc933a9f5574
baroneehud <baroneehud@gmail.com>
2016-06-28 16:40:15 +0300

    on timeout weird situation fixed. monitoring queue added

--- tasker/monitor/server/server.js
+++ tasker/monitor/server/server.js
@@ -1,9 +1,21 @@
+var bluebird = require('bluebird');
 var dgram = require('dgram');
 var express = require('express');
 var http = require('http');
 var socket_io = require('socket.io');
 var msgpack = require('msgpack-lite');
+var redis = require('redis');
 
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+
+var redis_client = redis.createClient(
+    {
+        'host': '127.0.0.1',
+        'port': 6380,
+    }
+);
 
 var udp_server_port = 33333;
 var udp_server_host = '0.0.0.0';
@@ -42,7 +54,6 @@ var Statistics = function () {
         var total_count = 0;
 
         this.statistics.last_log[rate].unshift(this.statistics.counter[rate]);
-        console.log(this.statistics.last_log[rate]);
 
         for (var i = 0; i < this.statistics.last_log[rate].length - 1; i++) {
             total_count += this.statistics.last_log[rate][i] - this.statistics.last_log[rate][i + 1];
@@ -90,6 +101,64 @@ websockets_server.on(
                 socket.emit(
                     'statistics',
                     statistics.statistics
+                );
+            }
+        );
+        socket.on(
+            'queues',
+            function (data) {
+                redis_client
+                .multi()
+                .keys('*')
+                .execAsync()
+                .then(
+                    function(result) {
+                        var queues_names = result[0];
+                        var queues_len_promises = [];
+
+                        for (var i = 0; i < queues_names.length; i++) {
+                            var llen_promise = null;
+                            var queue_name = queues_names[i];
+
+                            if (queue_name.indexOf('.results') > -1) {
+                                llen_promise = redis_client.scardAsync(
+                                    queue_name
+                                );
+                            } else if (queue_name.indexOf(':') === -1) {
+                                llen_promise = redis_client.llenAsync(
+                                    queue_name
+                                );
+                            } else {
+                                continue;
+                            }
+
+                            queues_len_promises.push(
+                                new bluebird.Promise(
+                                    function(resolve, reject) {
+                                        return llen_promise.then(
+                                            function(queue_name) {
+                                                return function(length) {
+                                                    return resolve(
+                                                        {
+                                                            'name': queue_name,
+                                                            'length': length
+                                                        }
+                                                    );
+                                                }
+                                            }(queue_name)
+                                        )
+                                    }
+                                )
+                            );
+                        }
+
+                        return bluebird.all(queues_len_promises);
+                    }
+                )
+                .then(
+                    function(results) {
+                        socket.emit('queues', results);
+                    }
                 );
             }
         );
